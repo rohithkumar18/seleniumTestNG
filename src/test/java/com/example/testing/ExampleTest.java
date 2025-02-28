cda05375f79ff9070f393afc900381cd4715772e
RohithKumar <kumarrohith784@gmail.com>
2025-03-01 00:47:44 +0530

    Moved ExampleTest to src/test/java

---
+++ src/test/java/com/example/testing/ExampleTest.java
@@ -0,0 +1,29 @@
+package com.example.testing;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExampleTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void openGoogle() {
+        driver.get("https://www.google.com");
+        System.out.println("Title: " + driver.getTitle());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
